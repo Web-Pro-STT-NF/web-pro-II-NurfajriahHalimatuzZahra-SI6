05ba144466de07c930710a4c25faf66cf32ef030
Nurfajriah Halimatuz Zahra <nurfajriahhz@gmail.com>
2022-03-05 17:15:08 +0700

    add praktikum-1

---
+++ praktikum-1/variable_user.php
@@ -0,0 +1,12 @@
+<?php
+    //definisikan variabel
+    $nama = "Nurfajriah Halimatuz Zahra";
+    $umur = 18;
+    $berat = 46;
+
+    echo 'Nama : ' . $nama;
+    echo '<br/>Umur : ' . $umur.' Tahun';
+    echo '<br/>Berat : ' . $berat.' Kg';
+
+    echo "<br/>Hello $nama Apa kabar?";
+?>
